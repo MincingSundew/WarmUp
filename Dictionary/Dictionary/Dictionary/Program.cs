878d1e27c9fdec2666b09ba5794956ff8dec1a0c
MincingSundew <sharmila.pichai@gmail.com>
2017-01-01 00:51:46 -0500

    Dictionaries and Maps

---
+++ Dictionary/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dictionary
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int total = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string, string> PhoneList = new Dictionary<string, string>();
+            for (int i =0; i<=total-1; i++)
+            {
+                string[] addName = Console.ReadLine().ToString().Split(' ');
+               
+                PhoneList.Add(addName[0], addName[1]);               
+            }
+            string input;
+            
+            do
+            {
+                string Output = null;
+
+                input = Console.ReadLine();
+                if (PhoneList.ContainsKey(input))
+                {
+                    PhoneList.TryGetValue(input, out Output);
+                    Console.WriteLine(input + "=" + Output);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            } while (input != null);
+
+
+        }
+    }
+}
