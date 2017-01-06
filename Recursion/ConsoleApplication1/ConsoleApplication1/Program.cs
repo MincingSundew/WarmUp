b554a5fcc4f29dd77c866d642b8a4335a4734ea9
MincingSundew <sharmila.pichai@gmail.com>
2017-01-06 17:48:07 -0500

    Adding Recursion

---
+++ Recursion/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+                {
+                return n * Factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+    }
+}
