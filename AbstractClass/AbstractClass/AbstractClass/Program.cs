92bb68c9599c45cfddd990bd7a68b6a4ca8eb10a
MincingSundew <sharmila.pichai@gmail.com>
2017-01-14 00:47:10 -0500

    SampleAbstract

---
+++ AbstractClass/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+abstract class Book
+{
+
+    protected String title;
+    protected String author;
+
+    public Book(String t, String a)
+    {
+        title = t;
+        author = a;
+    }
+    public abstract void display();
+
+
+}
+class MyBook:Book
+{
+    int price;
+    public MyBook(string mytitle, string myauthor, int myprice):base(mytitle, myauthor)
+        {
+        title = mytitle;
+        author = myauthor;
+        price = myprice;
+        }
+
+    override public void display()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Price: " + price);
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+    }
+}
