dc66d518c3c4d9f1c1e8550eab9d77fc7201888f
MincingSundew <sharmila.pichai@gmail.com>
2017-01-25 18:00:35 -0500

    AbsoluteDifference Positive Numbers Array

---
+++ AbstractClass/AbsoluteDifference/AbsoluteDifference/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteDifference
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+        public Difference(int[] elements)
+        {
+            this.elements = elements;
+        }
+        public int computeDifference()
+        {
+            int MaximumNumber = 0;
+            int MinimumNumber = 0;
+            for(int i=0;i<= maximumDifference-1; i++)
+            {
+                if(elements[i] > MaximumNumber)
+                {
+                    MaximumNumber = elements[i];
+                }
+                if (elements[i] < MinimumNumber)
+                {
+                    MinimumNumber = elements[i];
+                }
+
+                maximumDifference = MaximumNumber - MinimumNumber;
+            }
+            return maximumDifference;
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
