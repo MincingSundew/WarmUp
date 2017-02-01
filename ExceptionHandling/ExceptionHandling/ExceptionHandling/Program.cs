ccc2714ea1752c6fca4c718a1ce23f8916bae527
MincingSundew <sharmila.pichai@gmail.com>
2017-02-01 11:31:15 -0500

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            try
+            {
+              Console.WriteLine(Int32.Parse(S));
+            }
+            catch
+            {
+                Console.WriteLine("Bad String");
+            }
+
+        }
+    }
+}
