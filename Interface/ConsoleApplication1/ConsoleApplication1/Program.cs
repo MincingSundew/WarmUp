721fccbbeed9f2a3260310ae2ce7edeb84bcdd81
MincingSundew <sharmila.pichai@gmail.com>
2017-04-09 13:51:37 -0400

    Calculator

---
+++ Interface/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+       
+    }
+    class Calculator:AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            int sumDivisor = 0;
+            for(int i=1; i<= n; i++)
+            {
+                if(n%i==0)
+                {
+                    sumDivisor = sumDivisor +i;
+                }
+            }
+
+            return sumDivisor;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+           Console.WriteLine(c.divisorSum(8));
+            Console.ReadLine();
+        }
+    }
+}
