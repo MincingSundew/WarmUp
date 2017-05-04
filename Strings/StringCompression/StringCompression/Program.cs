a84ec3f19d27c6571d565cc08ff7d02fba7e2cce
MincingSundew <sharmila.pichai@gmail.com>
2017-05-04 19:04:04 -0400

    StringCompression

---
+++ Strings/StringCompression/StringCompression/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCompression
+{
+    class Program
+    {
+        public static string CompressBad(string s1)
+        {
+            string compressesString = "";
+            int countofChar = 1;
+
+
+            for (int i=0;i<s1.Length;i++)
+                {
+
+                if((i+1) >= s1.Length || s1[i] !=s1[i+1])
+                {
+                    compressesString = compressesString + s1[i]+countofChar;
+                    countofChar = 1;
+                }
+                else 
+                {
+                    countofChar++;
+                }
+            }
+            if (compressesString.Length < s1.Length)
+            {
+                return compressesString;
+            }
+            else
+            {
+                return s1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string compressString = Console.ReadLine();
+
+            string result = CompressBad(compressString);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
