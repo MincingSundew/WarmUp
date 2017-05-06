685c87c5fcaacdfc59b6bfe1f8c332511afcd25d
MincingSundew <sharmila.pichai@gmail.com>
2017-05-05 20:13:35 -0400

    String Rotation

---
+++ Strings/IsRotation/IsRotation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsRotation
+{
+    class Program
+    {
+
+        public static bool isRotation(string s1, string s2)
+        {
+            if (s2.Length == s2.Length && s1.Length > 0)
+            {
+                string s1s1 = s1 + s1;
+
+                return s1s1.Contains(s2);               
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            bool result = isRotation(s1, s2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
